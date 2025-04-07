f5dc2040e9342cda43b845d8fa7bc15e6ce3bf78
Kraprow <kamalat.g@nsru.ac.th>
2025-04-07 12:34:18 +0700

    update

--- resources/views/users/public_health/food_storage_license/pdf-form.blade.php
+++ resources/views/users/public_health/food_storage_license/pdf-form.blade.php
@@ -25,7 +25,7 @@
             font-size: 20px;
             margin: 0;
             padding: 0;
-            line-height: 1;
+            line-height: 0.8;
         }
 
 
@@ -36,6 +36,7 @@
 
         .title_doc {
             text-align: center;
+            font-weight: bold;
         }
 
         .box_text {
@@ -45,18 +46,7 @@
 
         .box_text span {
             display: inline-flex;
-            align-items: center;
             line-height: 1;
-        }
-
-        .box_text input[type="checkbox"] {
-            width: 17px;
-            /* ปรับขนาด checkbox ให้พอดีกับข้อความ */
-            height: 17px;
-            /* ปรับความสูงให้พอดีกับข้อความ */
-            margin-right: 5px;
-            margin-left: 5px;
-            /* เว้นระยะห่างระหว่าง checkbox และข้อความ */
         }
 
         .box_text_border {
@@ -69,21 +59,6 @@
             ;
         }
 
-        .box_text_border span {
-            display: inline-flex;
-            align-items: left;
-            line-height: 0.3;
-        }
-
-        .box_text_border input[type="checkbox"] {
-            width: 17px;
-            /* ปรับขนาด checkbox ให้พอดีกับข้อความ */
-            height: 17px;
-            /* ปรับความสูงให้พอดีกับข้อความ */
-            margin-right: 5px;
-            margin-left: 5px;
-            /* เว้นระยะห่างระหว่าง checkbox และข้อความ */
-        }
 
         .dotted-line {
             margin-left: 2px;
@@ -95,28 +70,169 @@
             /* รองรับ browser อื่น */
         }
 
-        .footer {
-            position: absolute;
-            /* ทำให้ footer ยึดที่ด้านล่าง */
-            bottom: -50px;
-            font-size: 23px;
-            /* ตั้งให้ footer อยู่ที่ด้านล่างสุดของหน้ากระดาษ */
-            width: 100%;
-            /* ให้ footer กว้างเต็มหน้ากระดาษ */
-            text-align: center;
-            /* จัดข้อความให้ตรงกลาง */
-            padding: 5px 0;
-            /* เพิ่มพื้นที่ด้านบนและล่างให้กับ footer */
-        }
-
     </style>
 </head>
 
 <body>
+    <div style="width: 100%; display: table;">
+        <!-- โลโก้อยู่ตรงกลาง -->
+        <div style="display: table-cell; width: 75%; text-align: center; vertical-align: top;">
+            <img src="data:image/png;base64,{{ base64_encode(file_get_contents(public_path('images/pdf/logo.png'))) }}"
+                alt="Logo" height="120" style="margin-right: -180px;">
+        </div>
 
-    <img src="data:image/png;base64,{{ base64_encode(file_get_contents(public_path('images/pdf/logo.png'))) }}"
-    alt="Logo" height="120">
-    <p>แบบคำร้องใบอณุญาตสะสมอาหาร</p>
-
+        <!-- กล่องข้อความอยู่ขวาสุด -->
+        <div style="display: table-cell; width: 25%; vertical-align: top; text-align: right;">
+            <div class="box_text_border" style="display: inline-block;">
+                <div class="box_text" style="text-align: left;">
+                    <span>ลงรับเลขที่</span>
+                    <span class="dotted-line" style="width: 85px;"></span> <br>
+                    <span>วันที่</span>
+                    <span class="dotted-line" style="width: 120px;"></span><br>
+                    <span>เวลา</span>
+                    <span class="dotted-line" style="width: 120px;"></span>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="title_doc" style="text-align:center;">
+        คำขอรับ ใบอนุญาต <br>
+        จัดตั้งสถานที่จำหน่ายอาหาร หรือ สถานที่สะสมอาหาร
+    </div>
+    <div class="box_text" style="text-align: right; margin-top:1rem;">
+        <span>เขียนที่</span>
+        <span class="dotted-line" style="width: 32%; text-align: center; line-height: 1;">องค์การบริหารส่วนตำบลคลองอุดมชลจร</span>
+    </div>
+    <div class="box_text" style="text-align: right; ">
+        <span>วันที่</span>
+        <span class="dotted-line" style="width: 5%; text-align: center;"></span>
+        <span>เดือน</span>
+        <span class="dotted-line" style="width: 15%; text-align: center;"> </span>
+        <span>พ.ศ.</span>
+        <span class="dotted-line" style="width: 10%; text-align: center;"> </span>
+    </div>
+    <div class="box_text" style="text-align: left; margin-left:5rem;">
+        <span>ข้าพเจ้า</span><input type="checkbox" style="margin: 0px 5px;"><span>บุคคลธรรมดา</span>
+        <input type="checkbox" style="margin: 0px 5px;"><span>นิติบุคคล ชื่อ</span>
+        <span class="dotted-line" style="width: 43%; text-align: center;"></span>
+        <span>อายุ</span>
+        <span class="dotted-line" style="width: 10%; text-align: center;"></span>
+        <span>ปี</span>
+    </div>
+    <div class="box_text" style="text-align: left;">
+        <span>สัญชาติ</span>
+        <span class="dotted-line" style="width: 10%; text-align: center;"></span>
+        <span>เลขบัตรประจำตัวประชาชน</span>
+        <span class="dotted-line" style="width: 17%; text-align: center;"></span>
+        <span>อยู่บ้าน/สำนักงานเลขที่</span>
+        <span class="dotted-line" style="width: 10%; text-align: center;"></span>
+        <span>หมู่ที่</span>
+        <span class="dotted-line" style="width: 10%; text-align: center;"></span>
+    </div>
+    <div class="box_text" style="text-align: left;">
+        <span>ตรอกซอย</span>
+        <span class="dotted-line" style="width: 22%; text-align: center;"></span>
+        <span>ถนน</span>
+        <span class="dotted-line" style="width: 22%; text-align: center;"></span>
+        <span>ตำบล/แขวง</span>
+        <span class="dotted-line" style="width: 22%; text-align: center;"></span>
+        <span>อำเภอ/เขต</span>
+        <span class="dotted-line" style="width: 20%; text-align: center;"></span>
+        <span>จังหวัด</span>
+        <span class="dotted-line" style="width: 20%; text-align: center;"></span>
+        <span>โทรศัพท์</span>
+        <span class="dotted-line" style="width: 19%; text-align: center;"></span>
+        <span>โทรสาร</span>
+        <span class="dotted-line" style="width: 18%; text-align: center;"></span>
+    </div>
+    <div class="box_text" style="text-align: left;">
+        <span>ขอยื่นคำร้องขอรับ/ขอต่ออายุใบอนุญาตจัดตั้งสถานที่ ต่อเจ้าพนักงานท้องถิ่น</span> <br>
+        <div style="margin-left:4rem;">
+            <input type="checkbox" style="margin: 0px 10px;"><span>จัดตั้งสถานที่จำหน่ายอาหาร</span>
+        <input type="checkbox" style="margin: 0px 10px;"><span>จัดตั้งสถานที่สะสมอาหาร</span>
+        </div>
+    </div>
+    <div class="box_text" style="text-align: left;">
+        <span>ตามใบอนุญาติ เล่มที่</span>
+        <span class="dotted-line" style="width: 12%; text-align: center;"></span>
+        <span>เลขที่</span>
+        <span class="dotted-line" style="width: 12%; text-align: center;"></span>
+        <span>ปี</span>
+        <span class="dotted-line" style="width: 12%; text-align: center;"></span>
+        <span>ซึ่งจะหมดอายุลงในวันที่</span>
+        <span class="dotted-line" style="width: 20%; text-align: center;"></span>
+    </div>
+    <div class="box_text" style="text-align: left; margin-left:5rem;">
+        <span>๑. สถานที่ชื่อ</span>
+        <span class="dotted-line" style="width: 40.5%; text-align: center;"></span>
+        <span>ประเภทร้าน</span>
+        <span class="dotted-line" style="width: 35%; text-align: center;"></span>
+    </div>
+    <div class="box_text" style="text-align: left; margin-left:5rem;">
+        <span>๒. สถานที่ตั้งเลขที่</span>
+        <span class="dotted-line" style="width: 15%; text-align: center;"></span>
+        <span>หมู่</span>
+        <span class="dotted-line" style="width: 15%; text-align: center;"></span>
+        <span>ตรอกซอย</span>
+        <span class="dotted-line" style="width: 15%; text-align: center;"></span>
+        <span>ถนน</span>
+        <span class="dotted-line" style="width: 18%; text-align: center;"></span>
+        <span>ตำบล</span>
+        <span class="dotted-line" style="width: 18%; text-align: center;"></span>
+        <span>อำเภอ</span>
+        <span class="dotted-line" style="width: 16%; text-align: center;"></span>
+        <span>จังหวัด</span>
+        <span class="dotted-line" style="width: 18%; text-align: center;"></span>
+        <span>โทรศัพท์</span>
+        <span class="dotted-line" style="width: 18%; text-align: center;"></span>
+        <span>โทรสาร</span>
+        <span class="dotted-line" style="width: 32%; text-align: center;"></span>
+        <span>พื้นที่ประกอบการ</span>
+        <span class="dotted-line" style="width: 33%; text-align: center;"></span>
+        <span>ตารางเมตร</span>
+        <span>จำนวนผู้ปรุง</span>
+        <span class="dotted-line" style="width: 6.8%; text-align: center;"></span>
+        <span>คน ผู้เสิร์ฟ</span>
+        <span class="dotted-line" style="width: 6.8%; text-align: center;"></span>
+        <span>คน รวมผู้สัมผัสอาหาร</span>
+        <span class="dotted-line" style="width: 6.8%; text-align: center;"></span>
+        <span>คน ผ่านการอบรมแล้วจำนวน</span>
+        <span class="dotted-line" style="width: 6.8%; text-align: center;"></span>
+        <span>คน</span>
+        <span>เปิดประกอบการตั้งแต่เวลา</span>
+        <span class="dotted-line" style="width: 16%; text-align: center;"></span>
+        <span>น. ถึงเวลา</span>
+        <span class="dotted-line" style="width: 16%; text-align: center;"></span>
+        <span>น. รวม</span>
+        <span class="dotted-line" style="width: 16%; text-align: center;"></span>
+        <span>ชั่วโมง/วัน</span>
+    </div>
+    <div class="box_text" style="text-align: left; margin-left:5rem; margin-top:-10px;">
+        <span>๓. พร้อมคำร้องนี้ข้าพเจ้าได้แนบหนังสือรับรองการแจ้งเดิมและเอกสารหลักฐานต่างๆ มาด้วย คือ</span> <br>
+        <div style="margin-left:0.5rem;">
+            <input type="checkbox" style="margin: 0px 10px;"><span>สำเนาบัตรประจำตัวประชาชนและสำเนาทะเบียนบ้านเจ้าของกิจการ</span> <br>
+            <input type="checkbox" style="margin: 0px 10px;"><span>สำเนาหนังสือรับรองการจดทะเบียนนิติบุคคลพร้อมสำเนาบัตรประชาชนของผู้แทนนิติบุคคล</span> <br>
+            <span style="margin-left: 2rem; margin-top:-10px;">(กรณีผู้ประกอบการเป็นนิติบุคคล)</span><br>
+            <input type="checkbox" style="margin: 0px 10px;"><span>หนังสือยินยอมให้ใช้สถานที่ / สัญญาเช่า</span> <br>
+            <input type="checkbox" style="margin: 0px 10px;"><span>หนังสือยินมอบอำนาจพร้อมสำเนาบัตรประชาชน / สำเนาทะเบียนบ้านผู้มอบ และผู้รับมอบอำนาจ</span> <br>
+            <input type="checkbox" style="margin: 0px 10px;"><span>ใบรับรองแพทย์ของผู้สัมผัสอาหาร (กรณีขอรับ / ขอต่ออายุใบอนุญาตแจ้งจัดตั้งสถานที่จำหน่ายอาหาร)</span> <br>
+            <input type="checkbox" style="margin: 0px 10px;"><span>ใบอนุญาตแจ้งจัดตั้งสถานที่จำหน่ายอาหาร หรือ สถานที่สะสมอาหารฉบับเดิม (ต้นฉบับ) (กรณีต่อ)</span> <br>
+            <input type="checkbox" style="margin: 0px 10px;"><span>แผนที่สถานที่ตั้งของสถานประกอบการ (กรณีขอรับ)</span> <br>
+            <input type="checkbox" style="margin: 0px 10px;"><span>อื่นๆ</span><span class="dotted-line" style="width: 20%; text-align: center;"></span> <br>
+        </div>
+        <span>ข้าพเจ้าขอรับรองว่า ข้อความในแบบคำขอนี้เป็นความจริงทุกประการ</span>
+    </div>
+    <div class="box_text" style="text-align: right; margin-top:1rem;">
+        <span>(ลงชื่อ)</span>
+        <span class="dotted-line" style="width: 35%; text-align: center;"> {{$form->full_name}}</span>
+        <span>ผู้ขอต่อ/ขอรับ/ผู้แจ้ง</span>
+        <div style="margin-right: 130px;">
+            <span>(</span>
+            <span class="dotted-line" style="width: 35%; text-align: center;"> {{$form->salutation}}{{$form->full_name}} </span>
+            <span>)</span>
+        </div>
+    </div>
 </body>
+
+
 </html>
